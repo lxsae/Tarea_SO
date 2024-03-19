d3039d067ac850db86e48d6774013c76cf7c1040
Norione R <83548015+lxsae@users.noreply.github.com>
2024-03-19 17:27:31 -0500

    Delete vagrantTarea02-definitiva directory

--- vagrantTarea02-definitiva/script_cliente.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp /vagrant/cliente.py /home/vagrant/
-sed -i 's/\r$//' /home/vagrant/cliente.py
-sudo chmod 755 /home/vagrant/cliente.py
--- vagrantTarea02-definitiva/script_server.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cp /vagrant/server.py /home/vagrant/
-sed -i 's/\r$//' /home/vagrant/server.py
-sudo chmod 755 /home/vagrant/server.py
